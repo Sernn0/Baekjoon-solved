dd059185a119d896342a8c4416f22e91fa55e20f
윤여명 <68244720+Sernn0@users.noreply.github.com>
2026-03-19 00:11:30 +0900

    [Bronze V] Title: 사칙연산, Time: 48 ms, Memory: 5504 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,9 @@
+string[] input = Console.ReadLine().Split(' ');
+int input1 = int.Parse(input[0]);
+int input2 = int.Parse(input[1]);
+
+Console.WriteLine(input1 + input2);
+Console.WriteLine(input1 - input2);
+Console.WriteLine(input1 * input2);
+Console.WriteLine(input1 / input2);
+Console.WriteLine(input1 % input2);
