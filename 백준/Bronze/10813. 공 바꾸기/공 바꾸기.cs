442e6a07839ed21a5434ca04b2ddd8daffa2cc9f
윤여명 <68244720+Sernn0@users.noreply.github.com>
2026-03-24 01:52:53 +0900

    [Bronze II] Title: 공 바꾸기, Time: 52 ms, Memory: 5960 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cs
@@ -0,0 +1,24 @@
+string[] input = Console.ReadLine().Split(' ');
+int N = int.Parse(input[0]);
+int M = int.Parse(input[1]);
+
+int[] bascket = new int[N];
+
+for (int i = 0; i < N; i++)
+{
+    bascket[i] = i + 1;
+}
+
+for (int times = 0; times < M; times++)
+{
+    string[] range = Console.ReadLine().Split(' ');
+    int i = int.Parse(range[0]) - 1;
+    int j = int.Parse(range[1]) - 1;
+
+    int temp = 0;
+    temp = bascket[i];
+    bascket[i] = bascket[j];
+    bascket[j] = temp;
+}
+
+Console.WriteLine(string.Join(" ", bascket));
