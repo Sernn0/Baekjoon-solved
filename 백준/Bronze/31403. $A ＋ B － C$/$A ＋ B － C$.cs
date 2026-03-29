87331e0031e10b646bc38e457469e6e26ce4ea7c
윤여명 <68244720+Sernn0@users.noreply.github.com>
2026-03-30 00:59:37 +0900

    [Bronze IV] Title: $A + B - C$, Time: 48 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cs
@@ -0,0 +1,6 @@
+string A = Console.ReadLine();
+string B = Console.ReadLine();
+string C = Console.ReadLine();
+
+Console.WriteLine(int.Parse(A) + int.Parse(B) - int.Parse(C));
+Console.WriteLine(int.Parse(A + B) - int.Parse(C));
